b2ca5cfa27f2ece737139596c529abf036fe45c4
murillous01 <murillous01@gmail.com>
2025-05-14 22:31:17 -0300

    escondendo parametro em team-member-card

--- src/components/ui/team-member-card.tsx
+++ src/components/ui/team-member-card.tsx
@@ -6,17 +6,17 @@ interface TeamMemberCardProps {
     name: string
     role: string
     specialty: string
-    image: string
+    /*image: string*/
   }
 }
 
 export default function TeamMemberCard({ member }: TeamMemberCardProps) {
-  const { name, role, specialty, image } = member
+  const { name, role, specialty/*, image*/ } = member
 
   return (
     <div className="flex flex-col items-center space-y-2 text-center">
       <div className="relative h-24 w-24 rounded-full overflow-hidden">
-        <Image src={"https://placehold.co/96x96"} width={96} height={96} alt={name} className="object-cover" />
+        <Image src={/*image ||*/ "https://placehold.co/96x96"} width={96} height={96} alt={name} className="object-cover" />
       </div>
       <div>
         <h4 className="font-bold">{name}</h4>
